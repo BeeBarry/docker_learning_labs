c667bb7951a3127dcfab27c698e3eb96239d9364
BeeBarry <barry.namdari@gmail.com>
2025-06-16 19:01:41 +0200

    Building middleware routes. Will try later with terminal curls and in Postman

---
+++ labs/express_container/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+const users = [];
+app.use(bodyParser.json());
+
+
+
+
+app.get('/', (req,res) => {
+    res.send('Hello good friends!');
+});
+
+// Get Requests
+app.get('/users', (req,res) => {
+    return res.json({users: users});
+});
+
+// Post requests
+
+app.post('/users', (req,res) => {
+    const newUserId = req.body.userId;
+    if (!newUserId) {
+        return res.status(400).send('missing userId');
+    }
+    if (users.includes(newUserId)) {
+        return res.status(400).send('User already exists.');
+    }
+    
+    users.push(newUserId);
+    return res.status(201).send('User now registered.')
+})
+
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
